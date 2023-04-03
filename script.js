ddcc966a1fe764ee37bd8fad0c1dc3f658ce0b8d
valdo pereira <>
2023-04-03 17:32:57 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+window.onload = function () {
+  const menu_btn = document.querySelector('.hamburger');
+  const mobile_menu = document.querySelector('.mobile-nav');
+
+  menu_btn.addEventListener('click', function() {
+    menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+  })
+
+}
+
+const inputs = document.querySelectorAll('.input');
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add('focus');
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if(this.value == '') {
+    parent.classList.remove('focus');
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener('focus', focusFunc);
+  input.addEventListener('blur', blurFunc);
+});
